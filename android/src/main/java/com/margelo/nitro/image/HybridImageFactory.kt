989efda88eebe38f18ac19c5777b811d54fc82ce
Marc Rousavy <me@mrousavy.com>
2025-06-10 18:09:20 +0200

    feat: Use Coil for loading! :)

--- android/src/main/java/com/margelo/nitro/image/HybridImageFactory.kt
+++ android/src/main/java/com/margelo/nitro/image/HybridImageFactory.kt
@@ -2,17 +2,30 @@ package com.margelo.nitro.image
 
 import android.annotation.SuppressLint
 import android.graphics.BitmapFactory
+import coil3.BitmapImage
+import coil3.ImageLoader
+import coil3.request.ImageRequest
+import com.facebook.react.bridge.ReactApplicationContext
 import com.margelo.nitro.NitroModules
 import com.margelo.nitro.core.Promise
-import java.net.URL
 
 class HybridImageFactory: HybridImageFactorySpec() {
+    private val context: ReactApplicationContext
+        get() = NitroModules.applicationContext ?: throw Error("No context - NitroModules.applicationContext was null!")
+    private val imageLoader = ImageLoader(context)
+
     override fun loadFromURL(url: String): Promise<HybridImageSpec> {
         return Promise.async {
-            val remoteURL = URL(url)
-            val stream = remoteURL.openStream()
-            val bitmap = BitmapFactory.decodeStream(stream)
-            return@async HybridImage(bitmap)
+            // 1. Create the Coil Request
+            val request = ImageRequest.Builder(context)
+                .data(url)
+                .build()
+            // 2. Execute it (async)
+            val result = imageLoader.execute(request)
+            val image = result.image ?: throw Error("Failed to load Image!")
+            // 3. Downcast to a Bitmap - if that fails, it might be an Animated Image...
+            val bitmap = image as? BitmapImage ?: throw Error("Requested Image is not a Bitmap!")
+            return@async HybridImage(bitmap.bitmap)
         }
     }
 
--- example/src/Images.ts
+++ example/src/Images.ts
@@ -1,8 +1,7 @@
 
 
 export function createImageURLs(count: number, size = 800): string[] {
-  return [...Array(count).fill(undefined)].map(() => {
-    const seed = Math.floor(Math.random() * 1000000);
-    return `https://picsum.photos/seed/${seed}/${size}`;
+  return [...Array(count).fill(undefined)].map((_, index) => {
+    return `https://picsum.photos/seed/${index}/${size}`;
   });
 }
