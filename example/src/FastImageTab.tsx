870db15c8eb37841a280b5fdbee9e03a15c564d1
Marc Rousavy <me@mrousavy.com>
2025-06-10 18:16:40 +0200

    Add FastImage example

--- example/src/FastImageTab.tsx
+++ example/src/FastImageTab.tsx
@@ -1,8 +1,29 @@
-import { Text, View } from 'react-native';
+import { useMemo } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { createImageURLs } from './Images';
+import FastImage from 'react-native-fast-image';
 
 
 export function FastImageTab() {
-  return <View>
-    <Text>FastImage Tab</Text>
-  </View>;
+  const imageURLs = useMemo(() => createImageURLs(100), []);
+
+  return (<View>
+    <Text>NitroImage Tab</Text>
+    <FlatList
+      data={imageURLs}
+      renderItem={({ item: url }) => (
+        <FastImage source={{ uri: url }} style={styles.image} />
+      )}
+    />
+  </View>);
 }
+
+
+const styles = StyleSheet.create({
+  image: {
+    width: '30%',
+    aspectRatio: 1,
+    borderWidth: 1,
+    borderColor: 'red',
+  },
+});
