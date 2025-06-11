cd97eb204b2f1fc1eb5ae4985ac4bc2326f8e9b2
Marc Rousavy <me@mrousavy.com>
2025-06-11 17:35:50 +0200

    feat: Implement `save` on Android

--- android/src/main/java/com/margelo/nitro/image/HybridImage.kt
+++ android/src/main/java/com/margelo/nitro/image/HybridImage.kt
@@ -5,6 +5,9 @@ import androidx.annotation.Keep
 import com.facebook.proguard.annotations.DoNotStrip
 import com.margelo.nitro.core.ArrayBuffer
 import com.margelo.nitro.core.Promise
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 @Suppress("ConvertSecondaryConstructorToPrimary")
 @Keep
@@ -42,5 +45,23 @@ class HybridImage: HybridImageSpec {
 
     override fun resizeAsync(width: Double, height: Double): Promise<HybridImageSpec> {
         return Promise.async { resize(width, height) }
+    }
+
+    override fun saveToFileAsync(
+        path: String,
+        format: ImageFormat,
+        quality: Double
+    ): Promise<Unit> {
+        return Promise.async {
+            bitmap.saveToFile(path, format, quality.toInt())
+        }
+    }
+
+    override fun saveToTemporaryFileAsync(format: ImageFormat, quality: Double): Promise<String> {
+        return Promise.async {
+            val tempFile = File.createTempFile("nitro_image_", format.name)
+            this.saveToFileAsync(tempFile.path, format, quality)
+            return@async tempFile.path
+        }
     }
 }
