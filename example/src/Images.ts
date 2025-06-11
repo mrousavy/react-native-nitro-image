03a2772cbbc1d009f50b4a64faae3c8b6738aba7
Marc Rousavy <me@mrousavy.com>
2025-06-11 20:18:17 +0200

    Load 1000 images

--- example/src/Images.ts
+++ example/src/Images.ts
@@ -1,5 +1,5 @@
 
-const DEFAULT_COUNT = 200;
+const DEFAULT_COUNT = 1000;
 
 export function createImageURLs(count: number = DEFAULT_COUNT, size = 800): string[] {
   return [...Array(count).fill(undefined)].map((_, index) => {
