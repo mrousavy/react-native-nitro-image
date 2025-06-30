c02fd6f398eb7e8df7f07e4c89637c15ccfaaaff
Marc Rousavy <me@mrousavy.com>
2025-06-30 18:41:24 +0200

    feat: Implement `crop(...)` on iOS

--- ios/HybridImage.swift
+++ ios/HybridImage.swift
@@ -36,6 +36,12 @@ class HybridImage: HybridImageSpec {
   }
   
   func resize(width: Double, height: Double) throws -> any HybridImageSpec {
+    guard width > 0 else {
+      throw RuntimeError.error(withMessage: "Width cannot be less than 0! (width: \(width))")
+    }
+    guard height > 0 else {
+      throw RuntimeError.error(withMessage: "Height cannot be less than 0! (height: \(height))")
+    }
     let targetSize = CGSize(width: width, height: height)
     
     let renderer = UIGraphicsImageRenderer(size: targetSize)
@@ -49,6 +55,34 @@ class HybridImage: HybridImageSpec {
   func resizeAsync(width: Double, height: Double) throws -> Promise<any HybridImageSpec> {
     return Promise.async {
       return try self.resize(width: width, height: height)
+    }
+  }
+  
+  func crop(startX: Double, startY: Double, endX: Double, endY: Double) throws -> any HybridImageSpec {
+    let targetWidth = endX - startX
+    let targetHeight = endY - startY
+    guard targetWidth > 0 else {
+      throw RuntimeError.error(withMessage: "Width cannot be less than 0! (startX: \(startX) - endX: \(endX) = \(width))")
+    }
+    guard targetHeight > 0 else {
+      throw RuntimeError.error(withMessage: "Height cannot be less than 0! (startY: \(startY) - endY: \(endY) = \(height))")
+    }
+    guard let cgImage = uiImage.cgImage else {
+      throw RuntimeError.error(withMessage: "This image does not have an underlying .cgImage!")
+    }
+    
+    let targetRect = CGRect(origin: CGPoint(x: startX, y: startY),
+                            size: CGSize(width: width, height: height))
+    guard let croppedCgImage = cgImage.cropping(to: targetRect) else {
+      throw RuntimeError.error(withMessage: "Failed to crop CGImage to \(targetRect)!")
+    }
+    let croppedUiImage = UIImage(cgImage: croppedCgImage)
+    return HybridImage(uiImage: croppedUiImage)
+  }
+  
+  func cropAsync(startX: Double, startY: Double, endX: Double, endY: Double) throws -> Promise<any HybridImageSpec> {
+    return Promise.async {
+      return try self.crop(startX: startX, startY: startY, endX: endX, endY: endY)
     }
   }
   
