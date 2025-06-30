28200158000bcc1dbbce347e6b10b4d11d7062e6
Marc Rousavy <me@mrousavy.com>
2025-06-30 18:30:43 +0200

    fix: Make `toArrayBuffer()` return raw RGBA data on iOS too (#7)
    * fix: Make `toArrayBuffer()` return raw RGBA data on iOS too
    
    * feat: Add some docs
    
    * use ARGB on iOS too

--- ios/HybridImage.swift
+++ ios/HybridImage.swift
@@ -27,10 +27,7 @@ class HybridImage: HybridImageSpec {
   }
   
   func toArrayBuffer() throws -> ArrayBufferHolder {
-    guard let data = uiImage.pngData() else {
-      throw RuntimeError.error(withMessage: "Failed to convert UIImage to png data!")
-    }
-    return try ArrayBufferHolder.copy(data: data)
+    return try uiImage.toRawRgbaArrayBuffer()
   }
   func toArrayBufferAsync() throws -> Promise<ArrayBufferHolder> {
     return Promise.async {
--- src/specs/Image.nitro.ts
+++ src/specs/Image.nitro.ts
@@ -7,22 +7,47 @@ export interface Image
   readonly width: number
   readonly height: number
 
+  /**
+   * Returns an array buffer containing the raw pixel data of the Image.
+   * Raw pixel data is always in `ARGB` format;
+   * ```
+   * [
+   *   A1, R1, G1, B1,
+   *   A2, R2, G2, B2,
+   *   ...
+   * ]
+   * ```
+   */
   toArrayBuffer(): ArrayBuffer
   toArrayBufferAsync(): Promise<ArrayBuffer>
 
+  /**
+   * Resizes this Image into a new image with the new given {@linkcode width} and {@linkcode height}.
+   */
   resize(width: number, height: number): Image
   resizeAsync(width: number, height: number): Promise<Image>
 
+  /**
+   * Saves this image in the given {@linkcode ImageFormat} to the given {@linkcode path}.
+   */
   saveToFileAsync(
     path: string,
     format: ImageFormat,
     quality: number
   ): Promise<void>
+  /**
+   * Saves this image in the given {@linkcode ImageFormat} to a temporary file, and return it's path.
+   */
   saveToTemporaryFileAsync(
     format: ImageFormat,
     quality: number
   ): Promise<string>
 
+  /**
+   * Encodes this Image into a ThumbHash.
+   * To convert the returned ThumbHash to a string, use `thumbHashToBase64String(...)`.
+   * @note To keep this efficient, {@linkcode resize} this image to a small size (<100x100) first.
+   */
   toThumbHash(): ArrayBuffer
   toThumbHashAsync(): Promise<ArrayBuffer>
 }
