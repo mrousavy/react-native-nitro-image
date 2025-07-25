651b10d39aa3f19c2799d867dd1b13ca8b332d79
Marc Rousavy <me@mrousavy.com>
2025-07-25 15:08:16 +0200

    Create scripts

---
+++ scripts/release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+echo "Starting the release process..."
+echo "Provided options: $@"
+
+echo "Publishing 'react-native-nitro-image' to NPM"
+cd packages/react-native-nitro-image
+bun release $@
+
+echo "Publishing 'react-native-nitro-web-image' to NPM"
+cd packages/react-native-nitro-web-image
+bun release $@
+
+echo "Creating a Git bump commit and GitHub release"
+cd ../..
+bun run release-it $@
+
+echo "Successfully released NitroImage!"
