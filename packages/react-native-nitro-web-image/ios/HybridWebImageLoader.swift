eacd745e555b7e2aca1ab2bde0309e950011b4ad
Marc Rousavy <me@mrousavy.com>
2025-07-28 12:20:15 +0200

    Implement it

--- packages/react-native-nitro-web-image/ios/HybridWebImageLoader.swift
+++ packages/react-native-nitro-web-image/ios/HybridWebImageLoader.swift
@@ -19,14 +19,27 @@ class HybridWebImageLoader: HybridWebImageLoaderSpec {
   }
   
   func loadImage() throws -> Promise<(any HybridImageSpec)> {
-    return Promise.rejected(withError: RuntimeError.error(withMessage: "Not yet implemented!"))
+    return Promise.async {
+      let webImageOptions = self.options?.toSDWebImageOptions() ?? []
+      let uiImage = try await SDWebImageManager.shared.loadImage(with: self.url, options: webImageOptions)
+      throw RuntimeError.error(withMessage: "I don't have the HybridImage type yet...")
+    }
   }
   
-  func requestImage(forView: (any HybridNitroImageViewSpec)) throws {
-    throw RuntimeError.error(withMessage: "Not yet implemented!")
+  func requestImage(forView view: (any HybridNitroImageViewSpec)) throws {
+    guard let view = view as? NativeImageView else { throw RuntimeError.error(withMessage: "Invalid view type!") }
+    
+    let webImageOptions = options?.toSDWebImageOptions() ?? []
+    view.imageView.sd_setImage(with: url,
+                               placeholderImage: nil,
+                               options: webImageOptions,
+                               context: nil)
   }
   
-  func dropImage(forView: (any HybridNitroImageViewSpec)) throws {
-    throw RuntimeError.error(withMessage: "Not yet implemented!")
+  func dropImage(forView view: (any HybridNitroImageViewSpec)) throws {
+    guard let view = view as? NativeImageView else { throw RuntimeError.error(withMessage: "Invalid view type!") }
+    
+    // TODO: Do we need to reset the image here or not?
+    view.imageView.image = nil
   }
 }
