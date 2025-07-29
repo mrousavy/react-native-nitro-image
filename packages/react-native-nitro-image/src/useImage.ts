13ef6684c5c5f966c8714f7ee813c5b1dfaab308
Marc Rousavy <me@mrousavy.com>
2025-07-29 20:43:49 +0200

    feat: Add extra `loadImage(...)` and `createImageLoader(...)` functions

--- packages/react-native-nitro-image/src/useImage.ts
+++ packages/react-native-nitro-image/src/useImage.ts
@@ -1,54 +1,7 @@
 import { useEffect, useState } from "react";
-import { Image as RNImage } from "react-native";
-import {
-    type AsyncImageSource,
-    isHybridImage,
-    isHybridImageLoader,
-    isHybridObject,
-} from "./AsyncImageSource";
-import { Images } from "./Images";
-import { OptionalWebImages } from "./OptionalWebLoader";
+import { type AsyncImageSource, isHybridObject } from "./AsyncImageSource";
+import { loadImage } from "./loadImage";
 import type { Image } from "./specs/Image.nitro";
-
-function createLoader(
-    source: AsyncImageSource,
-): (() => Promise<Image>) | undefined {
-    if (typeof source === "number") {
-        // It's a require(...) - a `number` which we need to resolve first
-        const resolvedSource = RNImage.resolveAssetSource(source);
-        if (__DEV__) {
-            // In debug, assets are streamed over the network
-            return createLoader({ url: resolvedSource.uri });
-        } else {
-            // In release, assets are resource IDs
-            return createLoader({ resource: resolvedSource.uri });
-        }
-    } else if (isHybridImage(source)) {
-        // don't do anything if this already is a HybridImage
-        return;
-    } else if (isHybridImageLoader(source)) {
-        // It's an ImageLoader
-        return () => source.loadImage();
-    } else if ("filePath" in source) {
-        // It's a { filePath }
-        return () => Images.loadFromFileAsync(source.filePath);
-    } else if ("arrayBuffer" in source) {
-        // It's a { arrayBuffer }
-        return () => Images.loadFromArrayBufferAsync(source.arrayBuffer);
-    } else if ("resource" in source) {
-        // It's a { resource }
-        return () => Images.loadFromResourcesAsync(source.resource);
-    } else if ("symbolName" in source) {
-        // It's a { symbolName }
-        return () => Promise.resolve(Images.loadFromSymbol(source.symbolName));
-    } else if ("url" in source) {
-        // It's a { url }
-        return () =>
-            OptionalWebImages.loadFromURLAsync(source.url, source.options);
-    } else {
-        throw new Error(`Unknown Image source! ${JSON.stringify(source)}`);
-    }
-}
 
 /**
  * A hook to asynchronously load an image from the
@@ -63,11 +16,11 @@ export function useImage(source: AsyncImageSource): Image | undefined {
 
     // biome-ignore lint: The dependencies array is a bit hacky.
     useEffect(() => {
-        const loader = createLoader(source);
-        if (loader != null) {
-            loader().then((i) => setImage(i));
-        }
+        (async () => {
+            const result = await loadImage(source);
+            setImage(result);
+        })();
     }, [isHybridObject(source) ? source : JSON.stringify(source)]);
 
-    return isHybridImage(source) ? source : image;
+    return image;
 }
--- packages/react-native-nitro-image/src/useImageLoader.ts
+++ packages/react-native-nitro-image/src/useImageLoader.ts
@@ -1,13 +1,6 @@
 import { useMemo } from "react";
-import { Image as RNImage } from "react-native";
-import {
-    type AsyncImageSource,
-    isHybridImage,
-    isHybridImageLoader,
-    isHybridObject,
-} from "./AsyncImageSource";
-import { ImageLoaders } from "./ImageLoaders";
-import { OptionalWebImages } from "./OptionalWebLoader";
+import { type AsyncImageSource, isHybridObject } from "./AsyncImageSource";
+import { createImageLoader } from "./createImageLoader";
 import type { Image } from "./specs/Image.nitro";
 import type { ImageLoader } from "./specs/ImageLoader.nitro";
 
@@ -15,42 +8,8 @@ export function useImageLoader(
     source: AsyncImageSource,
 ): Image | ImageLoader | undefined {
     // biome-ignore lint: The dependencies array is a bit hacky.
-    return useMemo<Image | ImageLoader | undefined>(() => {
-        if (typeof source === "number") {
-            // It's a require(...) - a `number` which we need to resolve first
-            const resolvedSource = RNImage.resolveAssetSource(source);
-            if (__DEV__) {
-                // In debug, assets are streamed over the network
-                return OptionalWebImages.createWebImageLoader(
-                    resolvedSource.uri,
-                );
-            } else {
-                // In release, assets are resource IDs
-                return ImageLoaders.createResourceImageLoader(
-                    resolvedSource.uri,
-                );
-            }
-        } else if (isHybridImage(source)) {
-            return source;
-        } else if (isHybridImageLoader(source)) {
-            return source;
-        } else if ("filePath" in source) {
-            return ImageLoaders.createFileImageLoader(source.filePath);
-        } else if ("arrayBuffer" in source) {
-            return ImageLoaders.createArrayBufferImageLoader(
-                source.arrayBuffer,
-            );
-        } else if ("resource" in source) {
-            return ImageLoaders.createResourceImageLoader(source.resource);
-        } else if ("symbolName" in source) {
-            return ImageLoaders.createSymbolImageLoader(source.symbolName);
-        } else if ("url" in source) {
-            return OptionalWebImages.createWebImageLoader(
-                source.url,
-                source.options,
-            );
-        } else {
-            return undefined;
-        }
-    }, [isHybridObject(source) ? source : JSON.stringify(source)]);
+    return useMemo<Image | ImageLoader | undefined>(
+        () => createImageLoader(source),
+        [isHybridObject(source) ? source : JSON.stringify(source)],
+    );
 }
