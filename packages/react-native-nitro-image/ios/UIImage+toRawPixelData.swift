8a55935cf1df925e64776848855c94d6e5d633ce
Marc Rousavy <me@mrousavy.com>
2025-10-22 13:22:11 +0200

    fix format

--- packages/react-native-nitro-image/ios/UIImage+toRawPixelData.swift
+++ packages/react-native-nitro-image/ios/UIImage+toRawPixelData.swift
@@ -23,35 +23,54 @@ extension UIImage {
     let bytesPerPixel = 4
     let bytesPerRow = bytesPerPixel * width
     let bitsPerComponent = 8
-    let pixelFormat = cg.pixelFormat
+    
+    let totalBytesSize = width * height * bytesPerPixel
+    
+    let is32Bit = cg.bitsPerComponent == 8 && cg.bitsPerPixel == 32
+    let isTight = cg.bytesPerRow == width * bytesPerPixel
+    if is32Bit, isTight,
+       let cfData = cg.dataProvider?.data,
+       let sourceData = CFDataGetBytePtr(cfData) {
+      // FAST PATH: Perform a direct copy if it's tightly packed memory
+      assert(CFDataGetLength(cfData) == totalBytesSize, "CFData is not the same length as our computed size!")
+      let arrayBuffer = ArrayBuffer.allocate(size: totalBytesSize)
+      memcpy(arrayBuffer.data, sourceData, totalBytesSize)
 
-    // Allocate a Data buffer of the right size
-    let totalSize = width * height * bytesPerPixel
-    let arrayBuffer = ArrayBuffer.allocate(size: totalSize)
-
-    // Create a RGB-premultiplied-first context (aka BGRA on little-endian iOS)
-    let colorSpace = CGColorSpaceCreateDeviceRGB()
-    let bitmapInfo = CGImageAlphaInfo.premultipliedFirst.rawValue
-
-    guard let ctx = CGContext(
-      data: arrayBuffer.data,
-      width: width,
-      height: height,
-      bitsPerComponent: bitsPerComponent,
-      bytesPerRow: bytesPerRow,
-      space: colorSpace,
-      bitmapInfo: bitmapInfo
-    ) else {
-      throw RuntimeError.error(withMessage: "Failed to create CGContext for \(width)x\(height) RGBA Image!")
+      // Pixel format is any 4-character format
+      let format = cg.pixelFormat
+      
+      return RawPixelData(buffer: arrayBuffer,
+                          width: Double(width),
+                          height: Double(height),
+                          pixelFormat: format)
+    } else {
+      // SLOW PATH: Perform a draw into a BGRA buffer.
+      let arrayBuffer = ArrayBuffer.allocate(size: totalBytesSize)
+      
+      // Create a RGB-premultiplied-first context (that's BGRA on iOS)
+      let colorSpace = CGColorSpaceCreateDeviceRGB()
+      let bitmapInfo = CGImageAlphaInfo.premultipliedFirst.rawValue | CGBitmapInfo.byteOrder32Little.rawValue
+      
+      guard let ctx = CGContext(
+        data: arrayBuffer.data,
+        width: width,
+        height: height,
+        bitsPerComponent: bitsPerComponent,
+        bytesPerRow: bytesPerRow,
+        space: colorSpace,
+        bitmapInfo: bitmapInfo
+      ) else {
+        throw RuntimeError.error(withMessage: "Failed to create CGContext for \(width)x\(height) RGBA Image!")
+      }
+      
+      // Draw the Image into the CGContext
+      let rect = CGRect(x: 0, y: 0, width: width, height: height)
+      ctx.draw(cg, in: rect)
+      
+      return RawPixelData(buffer: arrayBuffer,
+                          width: Double(width),
+                          height: Double(height),
+                          pixelFormat: .bgra)
     }
-
-    // Draw the Image into the CGContext
-    let rect = CGRect(x: 0, y: 0, width: width, height: height)
-    ctx.draw(cg, in: rect)
-
-    return RawPixelData(buffer: arrayBuffer,
-                        width: Double(width),
-                        height: Double(height),
-                        pixelFormat: pixelFormat)
   }
 }
