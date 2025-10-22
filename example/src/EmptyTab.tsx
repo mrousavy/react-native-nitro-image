930f7673b2ca5a092f69c8b7ace17eca7b9dc07a
Marc Rousavy <me@mrousavy.com>
2025-10-22 15:02:50 +0200

    Update EmptyTab.tsx

--- example/src/EmptyTab.tsx
+++ example/src/EmptyTab.tsx
@@ -1,35 +1,14 @@
-import { useEffect, useState } from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
-import { Images, NitroImage } from "react-native-nitro-image";
-import { WebImages } from "react-native-nitro-web-image";
+import { useState } from "react";
+import { StyleSheet, TextInput, View } from "react-native";
+import { NitroImage } from "react-native-nitro-image";
 
 export function EmptyTab() {
     const [value, setValue] = useState('https://picsum.photos/seed/123/600')
-    const [x, setX] = useState()
-
-    useEffect(() => {
-        (async () => {
-            try {
-            console.log('1')
-            const image = await WebImages.loadFromURLAsync(value)
-            console.log('2', image.width, image.height)
-            const rawData = await image.toRawPixelDataAsync()
-            console.log('3', rawData.buffer.byteLength)
-            setX(rawData)
-            const newImage = await Images.loadFromRawPixelDataAsync(rawData)
-            console.log('4', newImage.width, newImage.height)
-            } catch (e) {
-                console.error(e)
-            }
-        })()
-
-    }, [value])
 
     return (
         <View style={styles.container}>
             <TextInput placeholder="Image URL" value={value} onChangeText={setValue} style={styles.textInput} />
             <NitroImage image={{ url: value }} style={styles.image} />
-            {x != null && (<NitroImage image={{ rawPixelData: x }} style={styles.image} />)}
         </View>
     );
 }
@@ -45,13 +24,14 @@ const styles = StyleSheet.create({
         fontWeight: "500",
     },
     textInput: {
-        height: 50,
-        width: '100%'
+        borderWidth: 1,
+        borderRadius: 5,
+        paddingHorizontal: 10,
     },
     image: {
         width: 350,
         height: 350,
-        borderColor: 'grey',
-        borderWidth: StyleSheet.hairlineWidth
+        backgroundColor: 'grey',
+        marginTop: 15
     }
 });
