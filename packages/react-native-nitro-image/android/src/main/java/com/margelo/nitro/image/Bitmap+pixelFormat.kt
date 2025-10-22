d07c3523423e0ae9bd6dbfebb0639b8f640898b5
Marc Rousavy <me@mrousavy.com>
2025-10-22 16:23:38 +0200

    chore: Explain BGRA

--- packages/react-native-nitro-image/android/src/main/java/com/margelo/nitro/image/Bitmap+pixelFormat.kt
+++ packages/react-native-nitro-image/android/src/main/java/com/margelo/nitro/image/Bitmap+pixelFormat.kt
@@ -3,13 +3,24 @@ package com.margelo.nitro.image
 import android.graphics.Bitmap
 import android.hardware.HardwareBuffer
 import android.os.Build
+import java.nio.ByteOrder
 
 val Bitmap.pixelFormat: PixelFormat
     get() {
         when (config) {
             Bitmap.Config.ARGB_8888 -> {
-                // ARGB_8888 is BGRA on Android (because of little endian)
-                return PixelFormat.BGRA
+                // On Android, ARGB_8888 defines memory layout inside an Int, not the byte order.
+                // So where iOS would define Pixel Format ARGB as byte order [A, R, G, B],
+                // Android instead defines the Pixel Format ARGB as 0xAARRGGBB (32-bit Int) -
+                // which - if read on a little-endian machine, is reversed ([B, G, R, A]).
+                if (ByteOrder.nativeOrder() == ByteOrder.LITTLE_ENDIAN) {
+                    // almost every device nowadays is little endian
+                    return PixelFormat.BGRA
+                } else {
+                    // no devices use big endian anymore, but we keep this to highlight
+                    // why ARGB becomes BGRA on little endian.
+                    return PixelFormat.ARGB
+                }
             }
             Bitmap.Config.HARDWARE -> {
                 // Hardware Buffer is either RGBA or RGBX
