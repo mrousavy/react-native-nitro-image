38921fbc155c41d51516ab617e926899f3aff597
Marc Rousavy <me@mrousavy.com>
2026-02-10 11:12:32 +0100

    Update DispatchQueue+runOnMain.swift

--- packages/react-native-nitro-image/ios/Extensions/DispatchQueue+runOnMain.swift
+++ packages/react-native-nitro-image/ios/Extensions/DispatchQueue+runOnMain.swift
@@ -8,7 +8,11 @@
 import Foundation
 
 extension DispatchQueue {
-  static func runOnMain() {
-
+  static func runOnMain(_ block: @escaping () -> Void) {
+    if Thread.isMainThread {
+      block()
+    } else {
+      DispatchQueue.main.async(execute: block)
+    }
   }
 }
