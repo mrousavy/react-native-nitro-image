29432103d9a81ef133d8d4a56c517e09fcf90cd8
Marc Rousavy <me@mrousavy.com>
2026-02-10 11:39:50 +0100

    fix: Fix any load flickers on Android (#96)
    * fix: Fix any load flickers on Android
    
    * fix: Also support caching on Android
    
    * fix: Pass args
    
    * lint

--- packages/react-native-nitro-image/ios/HybridImageLoader.swift
+++ packages/react-native-nitro-image/ios/HybridImageLoader.swift
@@ -1,6 +1,6 @@
 //
 //  HybridImageLoader.swift
-//  Pods
+//  NitroImage
 //
 //  Created by Marc Rousavy on 25.07.25.
 //
@@ -9,28 +9,13 @@ import NitroModules
 
 class HybridImageLoader: HybridImageLoaderSpec {
   typealias LoadFunc = () throws -> Promise<any HybridImageSpec>
-  typealias RequestFunc = (_ for: any HybridNitroImageViewSpec) throws -> Void
-  typealias DropFunc = (_ for: any HybridNitroImageViewSpec) throws -> Void
 
   private let load: LoadFunc
-  private let requestImage: RequestFunc?
-  private let dropImage: DropFunc?
   private let allowCaching: Bool
   private var cachedResult: (any HybridImageSpec)? = nil
 
   init(load: @escaping LoadFunc, allowCaching: Bool = true) {
     self.load = load
-    self.requestImage = nil
-    self.dropImage = nil
-    self.allowCaching = allowCaching
-  }
-  init(load: @escaping LoadFunc,
-       requestImage: @escaping RequestFunc,
-       dropImage: @escaping DropFunc,
-       allowCaching: Bool = true) {
-    self.load = load
-    self.requestImage = requestImage
-    self.dropImage = dropImage
     self.allowCaching = allowCaching
   }
 
@@ -56,32 +41,20 @@ class HybridImageLoader: HybridImageLoaderSpec {
   }
 
   func requestImage(forView view: any HybridNitroImageViewSpec) throws {
-    if let requestImage {
-      // Custom requestImage(...) func supplied
-      return try requestImage(view)
-    } else {
-      // Default: Load & set image.
-      guard let view = view as? NativeImageView else { return }
-      try loadImage()
-        .then { image in
-          guard let image = image as? NativeImage else { return }
-          DispatchQueue.runOnMain {
-            view.imageView.image = image.uiImage
-          }
+    guard let view = view as? NativeImageView else { return }
+    try loadImage()
+      .then { image in
+        guard let image = image as? NativeImage else { return }
+        DispatchQueue.runOnMain {
+          view.imageView.image = image.uiImage
         }
-    }
+      }
   }
 
   func dropImage(forView view: any HybridNitroImageViewSpec) throws {
-    if let dropImage {
-      // Custom dropImage(...) func supplied
-      return try dropImage(view)
-    } else {
-      // Default: Set image to nil
-      guard let view = view as? NativeImageView else { return }
-      DispatchQueue.runOnMain {
-        view.imageView.image = nil
-      }
+    guard let view = view as? NativeImageView else { return }
+    DispatchQueue.runOnMain {
+      view.imageView.image = nil
     }
   }
 }
