e8d4fdfefb19dd8c0a6c4267bff25e590081f52d
Marc Rousavy <me@mrousavy.com>
2026-04-27 12:32:38 +0200

    chore: Upgrade Nitro to 0.35.5

--- packages/react-native-nitro-image/nitrogen/generated/android/kotlin/com/margelo/nitro/image/EncodedImageData.kt
+++ packages/react-native-nitro-image/nitrogen/generated/android/kotlin/com/margelo/nitro/image/EncodedImageData.kt
@@ -9,6 +9,7 @@ package com.margelo.nitro.image
 
 import androidx.annotation.Keep
 import com.facebook.proguard.annotations.DoNotStrip
+import java.util.Objects
 import com.margelo.nitro.core.ArrayBuffer
 
 /**
@@ -31,6 +32,24 @@ data class EncodedImageData(
   val imageFormat: ImageFormat
 ) {
   /* primary constructor */
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (other !is EncodedImageData) return false
+    return Objects.deepEquals(this.buffer, other.buffer)
+      && Objects.deepEquals(this.width, other.width)
+      && Objects.deepEquals(this.height, other.height)
+      && Objects.deepEquals(this.imageFormat, other.imageFormat)
+  }
+
+  override fun hashCode(): Int {
+    return arrayOf(
+      buffer,
+      width,
+      height,
+      imageFormat
+    ).contentDeepHashCode()
+  }
 
   companion object {
     /**
